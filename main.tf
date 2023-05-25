ff98bf7eaea61d438b9b8c437954bca3c0a373fd
anandreddy143 <81071205+anandreddy143@users.noreply.github.com>
2023-05-25 18:36:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
-provider "aws" {
-  source = "harshicop/aws"
-  version = "v4.67.0"
-  region  = "us-east-2"
+terrform{
+  required providers{
+  aws = {
+     source = "hashicorp/aws"
+     version = "v4.67.0"
+  }
 }
-
-
+}
+    
+provider "aws" {
+ region  = "us-east-1"
+}
 resource "aws_s3_bucket" "b" {
   bucket = "myawsbucket1290"
   acl    = "private"
